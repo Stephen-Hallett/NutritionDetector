cbc31347283c8651de2a8750d897a074835fea04
Stephen-Hallett <stevohallett@gmail.com>
2025-01-16 22:24:57 +1300

    fix: Convert app service plan to australia east since nz north doesnt work

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -16,25 +16,24 @@ resource "azurerm_storage_account" "sa" {
 
 resource "azurerm_service_plan" "asp" {
   name                = "asp-${var.project_id}-${var.env}-nzn-001"
-  location            = data.azurerm_resource_group.rg.location
+  location            = var.backup_location
   resource_group_name = data.azurerm_resource_group.rg.name
   os_type                = "Linux"
   sku_name = var.sku
 }
 
-resource "azurerm_function_app" "fa" {
+resource "azurerm_linux_function_app" "fa" {
   name                       = "fa-${var.project_id}-${var.env}-nzn-001"
   location                   = data.azurerm_resource_group.rg.location
   resource_group_name        = data.azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_service_plan.asp.id
+  service_plan_id        = azurerm_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  os_type                    = "linux"
-  version                    = "~4"
 
   site_config {
     cors {
       allowed_origins = ["*"]
     }
   }
+
 }
