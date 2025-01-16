4559c8e6fffc75c0df8d0caaefc0588fe415ab59
Stephen-Hallett <stevohallett@gmail.com>
2025-01-16 21:47:28 +1300

    fix: Correct app service plan reference

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_function_app" "fa" {
   name                       = "fa-${var.project_id}-${var.env}-nzn-001"
   location                   = data.azurerm_resource_group.rg.location
   resource_group_name        = data.azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  app_service_plan_id        = azurerm_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   os_type                    = "linux"
