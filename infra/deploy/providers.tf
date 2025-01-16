04ca1026b79be4ba17ccb86f980dd092b9b3098c
Stephen-Hallett <stevohallett@gmail.com>
2025-01-16 20:40:34 +1300

    fix: Use correct storage account name for backend

--- infra/deploy/providers.tf
+++ infra/deploy/providers.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "stterraformdeveau001"
+    storage_account_name = "stterraformdevnzn001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
