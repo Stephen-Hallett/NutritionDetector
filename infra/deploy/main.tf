e4faf33e3f4bbff61ac7668c262cd230810dfafd
Stephen-Hallett <stevohallett@gmail.com>
2025-01-16 21:39:56 +1300

    fix: Add data prefix to resource group references

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -8,16 +8,16 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "sa" {
   name                     = "sa-${var.project_id}-${var.env}-nzn-001"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-${var.project_id}-${var.env}-nzn-001"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   kind                = "Linux"
   reserved            = true
 
@@ -35,8 +35,8 @@ resource "azurerm_app_service_plan" "asp" {
 
 resource "azurerm_function_app" "fa" {
   name                       = "fa-${var.project_id}-${var.env}-nzn-001"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
   app_service_plan_id        = azurerm_app_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
