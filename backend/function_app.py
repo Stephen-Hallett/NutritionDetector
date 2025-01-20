dc76afe13e710cd1ec4e18a38c4ac0f835b1bb8e
Stephen-Hallett <stevohallett@gmail.com>
2025-01-20 22:12:34 +1300

    chore: Convert authentication level to admin

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,5 +1,4 @@
 import azure.functions as func
 from app import app as fastapi_app
 
-app = func.AsgiFunctionApp(
-    app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ADMIN)
